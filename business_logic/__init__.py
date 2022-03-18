ab780df8e8ea83418f4f88f7bdbb4f3691cc36f5
nicc <>
2022-03-18 15:26:11 +0100

    no message

--- business_logic/__init__.py
+++ business_logic/__init__.py
@@ -1,3 +1,4 @@
 import business_logic.main
 import business_logic.automl
-import business_logic.vision   #------> Fare si ca questo errore sia mostrato cachetools ora non lo mostra
+import business_logic.vision
+#------> Fare si ca questo errore sia mostrato cachetools ora non lo mostra
